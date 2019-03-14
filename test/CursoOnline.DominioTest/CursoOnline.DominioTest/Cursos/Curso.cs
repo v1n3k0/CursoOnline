d7c13c974717cfac38aeb54e39a20362300ed277
Rogério Salomão Ribeiro <rogerio.ribeiro@verity.com.br>
2019-03-14 10:55:44 -0300

    Aula 06

---
+++ test/CursoOnline.DominioTest/CursoOnline.DominioTest/Cursos/Curso.cs
@@ -0,0 +1,18 @@
+﻿namespace CursoOnline.DominioTest.Cursos
+{
+    internal class Curso
+    {
+        public string nome { get; private set; }
+        public double cargaHoraria { get; private set; }
+        public string publicoAlvo { get; private set; }
+        public double valor {get; private set; }
+
+        public Curso(string nome, double cargaHoraria, string publicoAlvo, double valor)
+        {
+            this.nome = nome;
+            this.cargaHoraria = cargaHoraria;
+            this.publicoAlvo = publicoAlvo;
+            this.valor = valor;
+        }
+    }
+}
---
+++ test/CursoOnline.DominioTest/CursoOnline.DominioTest/Cursos/CursoTeste.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace CursoOnline.DominioTest.Cursos
+{
+    public class CursoTeste
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            const string nome = "Informações básicas";
+            const int cargaHoraria = 80;
+            const string publicoAlvo = "Estudante";
+            const int valor = 950;
+
+            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+
+            Assert.Equal(nome, curso.nome);
+            Assert.Equal(cargaHoraria, curso.cargaHoraria);
+            Assert.Equal(publicoAlvo, curso.publicoAlvo);
+            Assert.Equal(valor, curso.valor);
+        }
+    }
+}
